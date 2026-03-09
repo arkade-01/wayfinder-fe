9595f668a06ca646e31cf0847010dcb25ae28091
arkade-01 <arkade@users.noreply.github.com>
2026-03-09 21:34:36 +0000

    add /beta redirect to typeform

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/beta",
+        destination: "https://form.typeform.com/to/xBMhn4nQ",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
